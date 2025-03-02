3aa5019aad20ac6ae1c61d22fecbcf27f6aba5db
shazii1 <shahzaibjahangir676@gmail.com>
2025-03-02 00:52:35 -0800

    Your commit message

--- AuthService/AuthService/Model/ApplicationDbContext.cs
+++ AuthService/AuthService/Model/ApplicationDbContext.cs
@@ -7,6 +7,7 @@ namespace AuthService.Model
 {
     public class ApplicationDbContext : DbContext
     {
+        //checkpush
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
 
         public DbSet<User> Users { get; set; }
--- AuthService/AuthService/obj/Debug/net8.0/AuthService.AssemblyInfo.cs
+++ AuthService/AuthService/obj/Debug/net8.0/AuthService.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuthService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+79e92b4376db500066bcc5eed56bbd3be12a3ccc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ab19131ad1bd193f09247fe1622cd8f4df9c1a5f")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuthService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuthService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
